b24b0d7a987f4ca1f8257a36cadeb22b6f610d1f
sachintesting <70683403+sachintesting@users.noreply.github.com>
2020-09-03 11:41:34 +0530

    Create Ec2_inatance

---
+++ Ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAJ6TCYUIL2RJ274BQ"
+  secret_key = "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"
+}
+resource "aws_instance" "ec2" {
+  ami = "ami-0ebc1ac48dfd14136"
+  instance_type = "t2.micro"
+}
+
+ 
+
+resource "aws_instance" "myec2" {
+  ami = "ami-0ebc1ac48dfd14136"
+  instance_type = "t2.micro"
